16715ff3033bf7f6987df5eabee3c4dac0fa11e4
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-11-29 12:22:42 +0300

    дз номер 3, задание 1

---
+++ ex1.3.py
@@ -0,0 +1,19 @@
+# дайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, 
+# стоящих на нечётной позиции.
+
+# Пример:
+
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+from random import randint
+
+numbers = []
+sum=0
+for i in range(10):
+    numbers.append(randint(-10, 10))
+print(numbers)
+for i in range(len(numbers)):
+    if i%2==1:
+        sum=sum+numbers[i]
+    i+=1
+print(sum)
+        
